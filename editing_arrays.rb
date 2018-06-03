da16ec5e7997f191f3a7792489d99adcbf045b9d
Sara Beth Ribot <sbribot@gmail.com>
2018-06-03 01:19:43 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -12,10 +12,12 @@ def change_rainbow_colors
   return rainbow_colors
 end 
 
-
-
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  rainbow_colors = []
+  rainbow_colors[0] = "red"
+  rainbow_colors[1] = "light_red"
+  rainbow_colors[2] = "light_yellow"
   rainbow_colors << "green"
   rainbow_colors << "blue"
   return rainbow_colors
