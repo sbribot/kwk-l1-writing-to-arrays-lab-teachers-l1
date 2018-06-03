7fc10188f5990ef162aee9326ab736dacc011452
Sara Beth Ribot <sbribot@gmail.com>
2018-06-03 01:19:07 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,12 +1,13 @@
-  rainbow_colors = [
-  "yellow",
-  "default",
-  "light_cyan"
-]
+# rainbow_colors = [
+#   "yellow",
+#   "default",
+#   "light_cyan"
+# ]
 
 def change_rainbow_colors
-  rainbow_colors[1] = "light_red"
+  rainbow_colors = []
   rainbow_colors[0] = "red"
+  rainbow_colors[1] = "light_red"
   rainbow_colors[2] = "light_yellow"
   return rainbow_colors
 end 
